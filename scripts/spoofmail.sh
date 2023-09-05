2f84907a0940cb81ff267aa64a2bc2eb71beff91
Chesapeake <git@moth.monster>
2023-09-05 01:06:51 -0400

    gaslight gatekeep girlboss

---
+++ scripts/spoofmail.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# spoofmail - gaslight cronie into giving you desktop notifications
+# made with <3 from moth.monster
+
+# SPDX-License-Identifier: WTFPL
+
+stdin="$(cat)" # don't call PETA
+
+commandName=$(echo "$stdin" | sed -n '3p' | cut -d ">" -f2-) # string magic
+commandOutput=$(echo "$stdin" | tail +16)
+
+XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send --icon=dialog-warning "Cronjob error:$commandName" "$commandOutput"
